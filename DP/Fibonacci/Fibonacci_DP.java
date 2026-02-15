a5257ea37b2d1e4893075bcffad2f267bf4b6f72
2310030428 <2310030428@klh.edu.in>
2026-02-15 20:49:37 +0530

    Create Fibonacci_DP.java

---
+++ DP/Fibonacci/Fibonacci_DP.java
@@ -0,0 +1,23 @@
+class Fibonacci_DP 
+{
+      public static int fib(int n) 
+    {
+          if (n <= 1)
+              return n;
+  
+          int prev2 = 0;
+          int prev1 = 1;
+          for (int i = 2; i <= n; i++) 
+          {
+              int curr = prev1 + prev2;
+              prev2 = prev1;
+              prev1 = curr;
+          }
+          return prev1;
+      }
+      public static void main(String[] args)
+    {
+          int n = 5;
+          System.out.println("Fibonacci of " + n + " is: " + fib(n));
+      }
+}
